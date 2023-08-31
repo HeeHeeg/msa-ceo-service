4c4142b5a5b0e5fcae8c7ffa55f0a46a2ad2aeb1
HeeHeeg <qmffndoden@naver.com>
2023-08-31 19:19:09 +0900

    msa

---
+++ study/playData/msa/ceo/src/main/java/com/example/ceo/domain/request/OwnerRequest.java
@@ -0,0 +1,18 @@
+package com.example.ceo.domain.request;
+
+import com.example.ceo.domain.entity.Owner;
+import com.example.ceo.domain.entity.Store;
+
+import java.util.UUID;
+
+public record OwnerRequest(UUID id, String name, String number) {
+
+    public Owner toEntity() {
+        return Owner.builder()
+                .id(id)
+                .name(name)
+                .number(number)
+                .build();
+
+    }
+}
---
+++ study/playData/msa/ceo/src/main/java/com/example/ceo/domain/response/OwnerResponse.java
@@ -0,0 +1,7 @@
+package com.example.ceo.domain.response;
+
+import java.util.UUID;
+
+public record OwnerResponse(UUID id, String name, String number) {
+
+}
